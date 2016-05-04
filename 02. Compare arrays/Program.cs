39c693fbc4f1371c363f0048a054f5754e037c45
mirko123 <mironikolov_vt@abv.bg>
2016-05-04 23:23:10 +0300

    3th

---
+++ 02. Compare arrays/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.Compare_arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] firstArr = new int[n];
+            int[] secondArr = new int[n];
+            string isEquals = "Equal";
+
+            for (int i = 0; i < n; i++)
+            {
+                firstArr[i] = int.Parse(Console.ReadLine());
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                secondArr[i] = int.Parse(Console.ReadLine());
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                if (firstArr[i] != secondArr[i])
+                {
+                    isEquals = "Not equal";
+                    break;
+                }
+            }
+            Console.WriteLine(isEquals);
+        }
+    }
+}
---
+++ 03. Compare char arrays/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.Compare_char_arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str1 = Console.ReadLine();
+            string str2 = Console.ReadLine();
+
+            if (str1.CompareTo(str2) > 0)
+            {
+                Console.WriteLine(">");
+            }
+            else if (str1.CompareTo(str2) == 0)
+            {
+                Console.WriteLine("=");
+            }
+            else
+            {
+                Console.WriteLine("<");
+            }
+        }
+    }
+}
